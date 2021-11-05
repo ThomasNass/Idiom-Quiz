e71ef32f45da2ef8b728da8049a5bd70115bdbc3
Thomas <tn900323@gmail.com>
2021-11-05 15:37:47 +0100

    The game is now playable

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const expressionParagraph = document.querySelector("#expression");
-const answerButtons = document.querySelectorAll(".answers")
+const buttonsDiv = document.querySelector("#buttonsDiv");
+const displayScore = document.querySelector("#score");
+const buttonNext = document.querySelector("#next");
+buttonNext.disabled = true;
 let score = 0;
+
 
 const getData = async () => {
     const response = await fetch("orden.json");
@@ -15,46 +19,61 @@ const func = async () => {
     const r2 = Math.floor(Math.random() * ExpressionsAndAnswers.length);
     const r3 = Math.floor(Math.random() * ExpressionsAndAnswers.length);
 
+
     let answers = [];
     expressionParagraph.textContent = ` ${ExpressionsAndAnswers[r].uttryck}`;
-    const right = ExpressionsAndAnswers[r].svar;
-    const wrong1 = ExpressionsAndAnswers[r1].svar;
-    const wrong2 = ExpressionsAndAnswers[r2].svar;
-    const wrong3 = ExpressionsAndAnswers[r3].svar;
+    let right = ExpressionsAndAnswers[r].svar;
+    let wrong1 = ExpressionsAndAnswers[r1].svar;
+    let wrong2 = ExpressionsAndAnswers[r2].svar;
+    let wrong3 = ExpressionsAndAnswers[r3].svar;
 
     answers.push(right, wrong1, wrong2, wrong3);
-    console.log(answers);
-
     shuffle(answers);
 
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] == right) {
+        const answerButton = document.createElement("button");
+        answerButton.classList.add("buttons");
 
-            answerButtons[i].addEventListener("click", () => {
-                answerButtons[i].classList.add("right");
+
+        if (answers[i] == right) {
+            answerButton.addEventListener("click", () => {
+                allButtons = document.querySelectorAll(".buttons");
+                answerButton.classList.add("right");
                 score++;
-                for (let i = 0; i < numberOfPlayers.length; i++) {
-                    numberOfPlayers[i].disabled = true;
+                displayScore.textContent = `Poäng:${score}`;
+                for (let j = 0; j < answers.length; j++) {
+                    allButtons[j].disabled = true;
+                    buttonNext.disabled = false;
                 }
             })
         }
         else {
 
-            answerButtons[i].addEventListener("click", () => {
-                answerButtons[i].classList.add("wrong");
-                for (let i = 0; i < answerButtons.length; i++) {
-                    answerButtons[i].disabled = true;
+            answerButton.addEventListener("click", () => {
+                allButtons = document.querySelectorAll(".buttons");
+                answerButton.classList.add("wrong");
+                for (let j = 0; j < answers.length; j++) {
+                    allButtons[j].disabled = true;
+                    buttonNext.disabled = false;
+
                 }
             })
         }
-        answerButtons[i].textContent = answers[i];
+        buttonsDiv.appendChild(answerButton);
+        answerButton.textContent = answers[i];
+
     }
 
-    console.log(answers);
 }
 
 
-
+buttonNext.addEventListener("click", () => {
+    while (buttonsDiv.firstChild) {
+        buttonsDiv.firstChild.remove();
+    }
+    buttonNext.disabled = true;
+    func();
+})
 
 func();
 const shuffle = (answers) => {
